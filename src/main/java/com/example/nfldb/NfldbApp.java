14c202595de3592389c9ae9e3a20043424e7e432
Abrar Chowdhury <abrar825@gmail.com>
2024-12-06 16:51:40 -0500

    DB Connection.

--- src/main/java/com/example/nfldb/NfldbApp.java
+++ src/main/java/com/example/nfldb/NfldbApp.java
@@ -2,11 +2,15 @@ package com.example.nfldb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.example.nfldb.controller.*;
 
 @SpringBootApplication
 class NfldbApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NfldbApp.class, args);
+
+		StatisticsController controller = new StatisticsController();
+		System.out.println(controller.getPassingStats());
 	}
 }
--- src/main/java/com/example/nfldb/repository/PassingRepository.java
+++ src/main/java/com/example/nfldb/repository/PassingRepository.java
@@ -3,6 +3,8 @@ package com.example.nfldb.repository;
 import com.example.nfldb.entity.PassingStats;
 import com.example.nfldb.entity.PlayerSeasonId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PassingRepository extends JpaRepository<PassingStats, PlayerSeasonId> {
 }
